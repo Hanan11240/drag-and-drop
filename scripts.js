19bf1d7d080293873a5abc1a4f5ef11f9b2ae2b3
Hanan112401 <118827480+Hanan112401@users.noreply.github.com>
2024-07-12 18:14:20 +0530

    initial commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const draggables = document.querySelectorAll('.draggable');
+const containers = document.querySelectorAll('.container');
+
+draggables.forEach((draggable,index)=>{
+    draggable.addEventListener('dragstart',()=>{
+        draggable.classList.add('dragging');
+    })
+
+    draggable.addEventListener('dragend',()=>{
+        draggable.classList.remove('dragging')
+    })
+})
+
+
+containers.forEach((container)=>{
+    container.addEventListener('dragover',(event)=>{
+        event.preventDefault();
+       const draggable = document.querySelector('.dragging');
+       container.appendChild(draggable)
+    })
+})
